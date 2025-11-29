81576032c447f5f899f933c8fec912c248b0b76f
Nelson <nelsonsomto10@gmail.com>
2025-11-29 11:12:20 +0100

    added carousel to portfolio page

--- asset/main.js
+++ asset/main.js
@@ -2,4 +2,35 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("dropdown-btn").addEventListener("click", () => {
     document.getElementById("drop-down").classList.toggle("active-dropdown");
   });
+
+  // Carousel
+  const slider = new Splide("#my-slider", {
+    type: "loop",
+    gap: "1rem",
+
+    // Desktop
+    perPage: 3,
+    arrows: false,
+    pagination: true,
+
+    // Tablet & Mobile
+    breakpoints: {
+      1024: {
+        perPage: 2,
+        pagination: false,
+        arrows: false,
+      },
+      600: {
+        perPage: 1,
+        pagination: false,
+        arrows: false,
+      },
+    },
+  });
+
+  slider.mount();
+
+  // Custom arrows for mobile + tablet
+  document.getElementById("nextBtn").onclick = () => slider.go(">");
+  document.getElementById("prevBtn").onclick = () => slider.go("<");
 });
